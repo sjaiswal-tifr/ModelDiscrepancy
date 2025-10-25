33f2c31249323dcd4c69f2cb7b2c7d6a1faff907
Sunil Jaiswal <118797201+sjaiswal-tifr@users.noreply.github.com>
2025-10-25 10:38:58 -0400

    updated plots

--- projects/arXiv_2509.19759/plot_scripts_heavyion.py
+++ projects/arXiv_2509.19759/plot_scripts_heavyion.py
@@ -143,6 +143,7 @@ def plot_eta_zeta(samples_eta, samples_zeta, eta_s, zeta_s,
 def plot_model_param(samples, mod_param_index, model_param_bounds, param_lab,
                      colors, linestyle, labels, alpha = 0.3, fig_name=None,
                      pos = [0.7, 0.6, 0.65, 0.6, 0.7, 0.65, 0.05, 0.7, 0.6]):
+    
     samp1, samp2 = samples[0][:, mod_param_index], samples[1][:, mod_param_index]
     mod_param_bonds = model_param_bounds[mod_param_index]
     ndim = len(mod_param_index)
@@ -224,8 +225,8 @@ def plot_model_param(samples, mod_param_index, model_param_bounds, param_lab,
 
 # ============================================================================================
 
-def plot_obs(experimental_data, obs_bins, quantiles_list, quantiles_error, colors, obs_labels, legends, 
-             fig_name=None, which = 'model'):
+def plot_obs(experimental_data, obs_bins, quantiles_list, colors, obs_labels, legends, fig_name=None, 
+             error = 'show', error_all = 'All', eps=1e-12):
 
     observables = list(obs_bins.keys())
     slices = []
@@ -261,22 +262,13 @@ def plot_obs(experimental_data, obs_bins, quantiles_list, quantiles_error, color
             ax.add_patch(rect)
         # ------------------------------------------------
 
-        if quantiles_error != None:
+        sep = 1.0/(len(quantiles_list)+1)  # fraction of local bin width for spacing
+        if error == 'show':
             # --- add sub-axis for error plot that shares x with main ax ---
             divider = make_axes_locatable(ax)
             ax_gp = divider.append_axes("bottom", size="30%", pad=0.05, sharex=ax)
             ax.tick_params(axis='x', labelbottom=False)     # x labels only on GP panel
-    
-            if which == 'model':
-                ax_gp.axhline(0, color="0.5", lw=0.8, zorder=0)
-                sep = 0.20 # fraction of local bin width for spacing
-            elif which == 'modelPlusGP':
-                ax_gp.axhline(1, color="0.5", lw=0.8, zorder=0)
-                sep = 0.30 # fraction of local bin width for spacing
-            # ax_gp.set_yticks([])
-            # ax_gp.set_ylabel(r'$\delta_\textrm{GP}/|\textrm{data}|$', labelpad=0.1)
-        elif quantiles_error == None:
-            sep = 0.20
+            ax_gp.axhline(0, color="0.5", lw=0.8, zorder=0)
         # ------------------------------------------------
         
         # plot model predictions and error plot ---------------------------------->
@@ -294,23 +286,32 @@ def plot_obs(experimental_data, obs_bins, quantiles_list, quantiles_error, color
             ax.errorbar(xj, median, yerr=yerr, fmt='o', color=color, lw=1, ms=2,
                         capsize=2, elinewidth=0.8, zorder=3, label=legends[j+1])
 
-            if quantiles_error != None:
-                # GP on sub-axis for the last two series (match colors & jitter)
-                if j >= m - len(quantiles_error):
-                    qgp = np.asarray(quantiles_error[j - (m - len(quantiles_error))])
-                    gmed, glo, ghi = qgp[0, slc], qgp[3, slc], qgp[4, slc]
-                    gyerr = np.vstack([gmed - glo, ghi - gmed])
-                    ax_gp.errorbar(xj, gmed, yerr=gyerr, fmt='o', color=color, lw=1, ms=2,
+            if error == 'show':
+                # bottom axis: residuals = experiment - model
+                resid = (exp_mean - median)/(exp_mean+eps)  # add 'eps' to avoid division by 0
+                resid_yerr = np.vstack([np.sqrt((median - lo)**2 + exp_std**2),
+                                        np.sqrt((hi - median)**2 + exp_std**2),
+                                       ])/(exp_mean+eps)
+                
+                if error_all == 'All':
+                    ax_gp.errorbar(xj, resid, yerr=resid_yerr, fmt='o', color=color, lw=1, ms=2,
                                    capsize=2, elinewidth=0.8, zorder=2)
+                    
+                else:
+                    # Show only the last `N` (= error_all) quantile results
+                    N = max(0, min(int(error_all), m))  # clamp to [0, m]
+                    if j >= m - N:
+                        ax_gp.errorbar(xj, resid, yerr=resid_yerr, fmt='o', color=color, lw=1, ms=2,
+                                       capsize=2, elinewidth=0.8, zorder=2)
     
         # labels/ticks (x on GP panel)
         ax.set_ylabel(obs_labels[obs], labelpad=3)
         ax.tick_params(axis='both', which='major')
         lastn = int(obs_bins[obs][-1][-1])
-        if quantiles_error != None:
+        if error == 'show':
             ax_gp.set_xticks(np.linspace(0, lastn, num=int(lastn/10) + 1))
             ax_gp.set_xlabel(r'\textrm{Centrality}', labelpad=0)
-        elif quantiles_error == None:
+        else:
             ax.set_xticks(np.linspace(0, lastn, num=int(lastn/10) + 1))
             ax.set_xlabel(r'\textrm{Centrality}', labelpad=0)
     
@@ -324,6 +325,109 @@ def plot_obs(experimental_data, obs_bins, quantiles_list, quantiles_error, color
     if fig_name is not None:
         plt.savefig(fig_name, format="pdf", dpi=400, bbox_inches="tight")
     plt.show()
+    
+# ============================================================================================
+
+# def plot_obs_error(experimental_data, obs_bins, quantiles_list, quantiles_error, colors, obs_labels, legends, 
+#              fig_name=None, which = 'model'):
+
+#     observables = list(obs_bins.keys())
+#     slices = []
+#     start_idx = 0
+#     for i, obs in enumerate(observables):
+#         n_bins_bayes = len(obs_bins[obs])
+#         end_idx = start_idx + n_bins_bayes
+#         slices.append((start_idx, end_idx))
+#         start_idx = end_idx
+    
+#     num_rows, num_cols = 3, 4
+#     fig, axes = plt.subplots(num_rows, num_cols, figsize=(18, 12),
+#                              sharex=False, gridspec_kw={'wspace': 0.25, 'hspace': 0.2})
+#     axes = axes.flatten()
+    
+#     for i, obs in enumerate(observables):
+#         ax = axes[i]
+#         bin_centers = np.array([np.mean(interval) for interval in obs_bins[obs]])
+#         bin_width  = np.array([y - interval[0] for y, interval in zip(bin_centers, obs_bins[obs])])  # half-width
+        
+#         slc = slice(slices[i][0], slices[i][1])
+    
+#         # plot experimental data ------------------------------------------------>
+#         exp_mean = experimental_data[slc, 0]
+#         exp_std  = experimental_data[slc, 1]
+#         ax.errorbar(bin_centers, exp_mean, xerr=bin_width, markersize=1, fmt='o',
+#                     color='black', label=legends[0])
+
+#         # shaded boxes for exp errors
+#         for (x_left, x_right), y, yerr in zip(obs_bins[obs], exp_mean, exp_std):
+#             rect = Rectangle((x_left, y - yerr), x_right - x_left, 2 * yerr,
+#                              edgecolor='black', facecolor='black', alpha=0.2)
+#             ax.add_patch(rect)
+#         # ------------------------------------------------
+
+#         if quantiles_error != None:
+#             # --- add sub-axis for error plot that shares x with main ax ---
+#             divider = make_axes_locatable(ax)
+#             ax_gp = divider.append_axes("bottom", size="30%", pad=0.05, sharex=ax)
+#             ax.tick_params(axis='x', labelbottom=False)     # x labels only on GP panel
+    
+#             if which == 'model':
+#                 ax_gp.axhline(0, color="0.5", lw=0.8, zorder=0)
+#                 sep = 0.20 # fraction of local bin width for spacing
+#             elif which == 'modelPlusGP':
+#                 ax_gp.axhline(1, color="0.5", lw=0.8, zorder=0)
+#                 sep = 0.30 # fraction of local bin width for spacing
+#             # ax_gp.set_yticks([])
+#             # ax_gp.set_ylabel(r'$\delta_\textrm{GP}/|\textrm{data}|$', labelpad=0.1)
+#         elif quantiles_error == None:
+#             sep = 0.20
+#         # ------------------------------------------------
+        
+#         # plot model predictions and error plot ---------------------------------->
+#         m = len(quantiles_list)            # 4 series total
+#         bin_widths = obs_bins[obs][:, 1] - obs_bins[obs][:, 0]  # full width (for jitter)
+        
+#         for j, (color, q_model) in enumerate(zip(colors[:m], quantiles_list)):
+#             q_model = np.asarray(q_model)
+#             median, lo, hi = q_model[0, slc], q_model[3, slc], q_model[4, slc]
+#             yerr = np.vstack([median - lo, hi - median])
+#             x_shift = (j - (m - 1) / 2) * sep * bin_widths
+#             xj = bin_centers + x_shift
+    
+#             # model on main axis
+#             ax.errorbar(xj, median, yerr=yerr, fmt='o', color=color, lw=1, ms=2,
+#                         capsize=2, elinewidth=0.8, zorder=3, label=legends[j+1])
+
+#             if quantiles_error != None:
+#                 # GP on sub-axis for the last two series (match colors & jitter)
+#                 if j >= m - len(quantiles_error):
+#                     qgp = np.asarray(quantiles_error[j - (m - len(quantiles_error))])
+#                     gmed, glo, ghi = qgp[0, slc], qgp[3, slc], qgp[4, slc]
+#                     gyerr = np.vstack([gmed - glo, ghi - gmed])
+#                     ax_gp.errorbar(xj, gmed, yerr=gyerr, fmt='o', color=color, lw=1, ms=2,
+#                                    capsize=2, elinewidth=0.8, zorder=2)
+    
+#         # labels/ticks (x on GP panel)
+#         ax.set_ylabel(obs_labels[obs], labelpad=3)
+#         ax.tick_params(axis='both', which='major')
+#         lastn = int(obs_bins[obs][-1][-1])
+#         if quantiles_error != None:
+#             ax_gp.set_xticks(np.linspace(0, lastn, num=int(lastn/10) + 1))
+#             ax_gp.set_xlabel(r'\textrm{Centrality}', labelpad=0)
+#         elif quantiles_error == None:
+#             ax.set_xticks(np.linspace(0, lastn, num=int(lastn/10) + 1))
+#             ax.set_xlabel(r'\textrm{Centrality}', labelpad=0)
+    
+#         if i == 0:
+#             ax.legend(fontsize=12, loc="upper right", frameon=False,
+#                       handlelength=1.0, handletextpad=0.2,   # tighter text next to handle
+#                       labelspacing=0.3,                      # more vertical space between entries
+#                       borderaxespad=0.2, columnspacing=0.6)
+    
+#     plt.tight_layout()
+#     if fig_name is not None:
+#         plt.savefig(fig_name, format="pdf", dpi=400, bbox_inches="tight")
+#     plt.show()
     
 # ============================================================================================
 
